7f9eb698b28170001a3988795f984861dcc85dd1
MORO-66t <scwtud@gmail.com>
2024-04-05 21:03:53 +0200

    fi

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -28,14 +28,14 @@ def fizzbuzz(n):
     print(" ".join(tmp_result))
 
 
-# if __name__ == '__main__':
-#     if len(sys.argv) <= 1:
-#         print("Missing number")
-#         print("Usage: ./0-fizzbuzz.py <number>")
-#         print("Example: ./0-fizzbuzz.py 89")
-#         sys.exit(1)
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
 
-#     number = int(sys.argv[1])
-#     fizzbuzz(number)
+    number = int(sys.argv[1])
+    fizzbuzz(number)
 
 fizzbuzz(50)
